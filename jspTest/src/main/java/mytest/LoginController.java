51a6443abdf0b2335da3ab219f86d25a5e3167a0
harshj-1703 <harshj6680@gmail.com>
2023-03-20 12:22:54 +0530

    update

---
+++ jspTest/src/main/java/mytest/LoginController.java
@@ -0,0 +1,24 @@
+package mytest;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/logincheck")
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uname = req.getParameter("username");
+        String password = req.getParameter("password");
+        if(uname.equalsIgnoreCase(password))
+        {
+            req.getRequestDispatcher("dashboard.jsp").forward(req,resp);
+        }
+    }
+}

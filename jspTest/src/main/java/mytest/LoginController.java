cbac2dd7e788faa8ac4b607a7bd51812d26bf51a
harshj-1703 <harshj6680@gmail.com>
2023-03-20 13:01:20 +0530

    update

--- jspTest/src/main/java/mytest/LoginController.java
+++ jspTest/src/main/java/mytest/LoginController.java
@@ -15,9 +15,17 @@ public class LoginController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String uname = req.getParameter("username");
         String password = req.getParameter("password");
-        if(uname.equalsIgnoreCase(password))
+        AuthUser user = new AuthUser();
+        user.setUsername(uname);
+        user.setPassword(password);
+        if(new AuthUserService().validate(user))
         {
+            req.setAttribute("userdetails",user);
             req.getRequestDispatcher("dashboard.jsp").forward(req,resp);
         }
+//        if(uname.equalsIgnoreCase(password))
+//        {
+//            req.getRequestDispatcher("dashboard.jsp").forward(req,resp);
+//        }
     }
 }
--- jspTest/src/main/webapp/dashboard.jsp
+++ jspTest/src/main/webapp/dashboard.jsp
@@ -1,8 +1,13 @@
+<%@page import="mytest.AuthUser"%>
 <html>
 <head>
 <title></title>
 </head>
 <body>
     this is dashboard
+    <%
+        AuthUser user = (AuthUser)request.getAttribute("userdetails"); %><br><%
+        out.println("Welcome "+user.getUsername());
+    %>
 </body>
 </html>

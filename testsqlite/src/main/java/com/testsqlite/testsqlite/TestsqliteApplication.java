f87fa6802652c8b5746dff8c6e3ea68cb08f56c6
harshj-1703 <harshj6680@gmail.com>
2023-04-15 13:00:47 +0530

    sqlite install dependency

---
+++ testsqlite/src/main/java/com/testsqlite/testsqlite/TestsqliteApplication.java
@@ -0,0 +1,13 @@
+package com.testsqlite.testsqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsqliteApplication.class, args);
+	}
+
+}
---
+++ testsqlite/src/test/java/com/testsqlite/testsqlite/TestsqliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testsqlite.testsqlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsqliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

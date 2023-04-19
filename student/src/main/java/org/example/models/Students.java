2732e02926c939a935b0756a5411ca9a92041a6d
harshj-1703 <harshj6680@gmail.com>
2023-04-19 20:39:19 +0530

    add

---
+++ student/src/main/java/org/example/models/Students.java
@@ -0,0 +1,49 @@
+package org.example.models;
+
+public class Students
+{
+    private static int uid;
+    private String uname;
+    private String password;
+    private int status;
+
+    public Students(int uid, String uname, String password,int status) {
+        this.uid = uid;
+        this.uname = uname;
+        this.password = password;
+        this.status = status;
+    }
+
+    public static int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+}
